ad09a6e6d1951794378e610c2d203ee1872cffc6
suhirtha <suhirtha03@gmail.com>
2017-01-05 19:58:30 +0530

    final changes

--- js/self-inc.js
+++ js/self-inc.js
@@ -3,98 +3,119 @@
 	    data: {
 	        show: true,
 	        showChannel: false,
-	        counter: 15,
+	        counter: 16,
 	        ok: false,
 	        notok: false,
 	        showSettings: false,
-	        programs: [{
+	        obj16: {
 	            "counter": 16,
-	            "name": "Star Movies",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel1.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	            
+	        },
+	        obj17: {
 	            "counter": 17,
-	            "name": "National Geo",
-	            "src":"videos/nat.mp4",
-	            "image": "img/channel2.png"
-	        }, {
+	            "src": "videos/nat.mp4",
+	            
+	        },
+	        obj18: {
 	            "counter": 18,
-	            "name": "Animax",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel3.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	            
+	        },
+	        obj19: {
 	            "counter": 19,
-	            "name": "Romedy Now",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel4.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	            
+	        },
+	        obj20: {
 	            "counter": 20,
-	            "name": "Movies Now",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel5.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	            
+	        },
+	        obj21: {
 	            "counter": 21,
-	            "name": "POGO",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel6.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	            
+	        },
+	        obj22: {
 	            "counter": 22,
-	            "name": "Disney",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel7.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	           
+	        },
+	        obj23: {
 	            "counter": 23,
-	            "name": "HBO",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel8.png"
-	        }, {
+	            "src": "videos/naruto.mp4",
+	            
+	        },
+	        obj24: {
 	            "counter": 24,
-	            "name": "Star Movies Action",
-	            "src":"videos/naruto.mp4",
-	            "image": "img/channel9.png"
-	        }],
-	        
+	            "src": "videos/naruto.mp4",
+	        },
+	        programs:[{
+	        	"counter": 16,
+	        	"name": "Star Movies"
+	        },
+	        {
+	        	"counter": 17,
+	            "name": "National Geo"
+	        },
+	        {
+	        	"counter": 18,
+	            "name": "Animax"
+	        },
+	        {
+	        	"counter": 19,
+	            "name": "Romedy Now"	
+	        },{
+	        	"counter": 20,
+	            "name": "Movies Now"
+	        },
+	        {
+	        	"counter": 21,
+	            "name": "POGO"
+	        },
+	        {
+	        	"counter": 22,
+	            "name": "Disney"
+	        },{
+	        	"counter": 23,
+	            "name": "HBO"
+	        },{
+	        	"counter": 24,
+	            "name": "Star Movies Action"
+	        }
+	        ]
+
+
 	    },
 	    methods: {
 	        switchOn: function() {
-	            if (this.show) {
-	                this.show = false;
-	                this.showChannel = true;
-	            } else {
-	                this.show = true;
-	                this.showChannel = false;
-	            }
+	            this.show = !this.show;
+	            this.showChannel = !this.showChannel;
 	        }
-
 	    }
 	})
-	var slider = new Slider('#bright-slide');
-	var slider = new Slider('#contrast-slide');
-	var slider = new Slider('#color-slide');
-	var myVideo = document.getElementById("video"); 
-
-	function playPause() { 
-		myVideo.width = 380; 
-	    if (myVideo.paused) 
-	        myVideo.play(); 
-	    else 
-	        myVideo.pause(); 
-
-	} 
-	
-
+	new Slider('#bright-slide');
+	new Slider('#contrast-slide');
+	new Slider('#color-slide');
+	var myVideo = document.getElementById("video");
+	function playPause() {
+	    if (myVideo.paused)
+	        myVideo.play();
+	    else
+	        myVideo.pause();
+	}
 	$('.vol-up').click(function() {
-        $('.volume').fadeIn().animate({ width: '+=10px' }, 'fast', function() {
-            setTimeout(function() {
-                $('.volume').fadeOut(100);
-            }, 1000);
-        });
-    });
-    $('.vol-down').click(function() {
-        $('.volume').fadeIn().animate({ width: '-=10px' }, 'fast', function() {
-            setTimeout(function() {
-                $('.volume').fadeOut(100);
-            }, 1000);
-        });
-    });
-    
+	    $('.volume').fadeIn().animate({ width: '+=10px' }, 'fast', function() {
+	        setTimeout(function() {
+	            $('.volume').fadeOut(100);
+	        }, 1000);
+	    });
+	});
+	$('.vol-down').click(function() {
+	    $('.volume').fadeIn().animate({ width: '-=10px' }, 'fast', function() {
+	        setTimeout(function() {
+	            $('.volume').fadeOut(100);
+	        }, 1000);
+	    });
+	});
